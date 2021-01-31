6acbb8e72b46ac9e3145295d886ad3534dbcdcea
Erika Tavera <dugarerika@gmail.com>
2021-01-31 16:12:27 +0300

    agrego codigo para el auth reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,3 +1,17 @@
-const reducer = (state, action) => state;
+import * as types from './types';
+const initialState = { auth: null, anuncios: null };
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case types.AUTH_LOGIN:
+			//login
+			return { ...state, auth: action.payload.loggedUser };
+		case types.AUTH_LOGOUT:
+			//logout
+			return { ...state, auth: null };
+		default:
+			return state;
+	}
+};
 
 export default reducer;
