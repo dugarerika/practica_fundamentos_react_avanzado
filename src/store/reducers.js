5d586fbef2343607e638b98d941f17b2fcfaa420
Erika Tavera <dugarerika@gmail.com>
2021-02-01 17:23:26 +0300

    refactor reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,49 +1,50 @@
 import * as types from './types';
 const initialState = { auth: null, anuncios: null };
 
-const reducer = (state = initialState, action) => {
+const auth = (state = initialState.auth, action) => {
 	switch (action.type) {
 		case types.AUTH_LOGIN:
-			//login
-			//return Object.assign({}, state,{auth:action.payload.loggedUser})
-			return { ...state, auth: action.payload.loggedUser };
+			return action.payload.loggedUser;
 
 		case types.AUTH_LOGOUT:
-			//logout
-			return { ...state, auth: null };
-		case types.ANUNCIOS_CREATED:
-			if (!state.anuncios) {
-				return {
-					...state,
-					anuncios: [
-						action.payload.anuncio
-					]
-				};
-			}
-			return {
-				...state,
-				anuncios: state.anuncios.concat(
-					action.payload.anuncio
-				)
-			};
-		case types.ANUNCIOS_DELETED:
-			if (!state.anuncios) {
-				return {
-					...state,
-					anuncios: [
-						action.payload.anuncio
-					]
-				};
-			}
-			return {
-				...state,
-				anuncios: state.anuncios.filter(
-					action.payload.anuncio
-				)
-			};
+			return null;
+
 		default:
 			return state;
 	}
+};
+
+const anuncios = (
+	state = initialState.anuncios,
+	action
+) => {
+	switch (action.type) {
+		case types.ANUNCIOS_CREATED:
+			if (!state) {
+				return [
+					action.payload.anuncio
+				];
+			}
+			return state.concat(action.payload.anuncio);
+
+		case types.ANUNCIOS_DELETED:
+			if (!state) {
+				return [
+					action.payload.anuncio
+				];
+			}
+			return state.filter(action.payload.anuncio);
+
+		default:
+			return state;
+	}
+};
+
+const reducer = (state, action) => {
+	return {
+		auth: auth(state.auth, action),
+		anuncios: anuncios(state.anuncios, action)
+	};
 };
 
 export default reducer;
